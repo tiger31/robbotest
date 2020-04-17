0f5260e40a9f263885733a2c5c26ed6a68ee75eb
Ilya Sergeev <tiger31839@gmail.com>
2020-04-17 17:05:13 +0300

    Fix syntax

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     ],
     dependency_links=[
         'git+https://github.com/edx/xblock-utils.git@v1.1.0#egg=xblock-utils==1.1.0'
-    ]
+    ],
     entry_points={
         'xblock.v1': [
             'robbotest = robbotest:RobboTestXBlock',
